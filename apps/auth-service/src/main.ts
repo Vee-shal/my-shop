f606c49b356231b90cba8db29788febedab87e53
vishal <vishal472005joshi@gmail.com>
2025-10-25 21:02:15 +0530

    Initial backend setup: DB models-users and images, error handling, services running

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-
+import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
+import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(
@@ -10,6 +11,11 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(errorMiddleware);
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello API" });
